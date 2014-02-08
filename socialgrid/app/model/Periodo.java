03318cd7af59897b60a0d541b4fd2f7ab96bdaf4
Martha Michelly MMs Bebe (BB) <martha@rabeca.(none)>
2014-02-08 17:11:37 -0300

    Escrevendo métodos classe Periodo

--- socialgrid/app/model/Periodo.java
+++ socialgrid/app/model/Periodo.java
@@ -1,21 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Periodo {
+	
+	List<Disciplina> disciplinas;
+	
+	public Periodo(){
+		disciplinas = new ArrayList<Disciplina>();
+		
+	}
 
 	public Set<Disciplina> getDisciplinas() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<Disciplina> disciplinasSet = new HashSet<Disciplina>(disciplinas);
+		return disciplinasSet;
+		
 	}
 
 	public void adicionaDisciplina(Disciplina disciplina) {
-		// TODO Auto-generated method stub
+		disciplinas.add(disciplina);
 		
 	}
 
 	public void removeDisciplina(Disciplina disciplina) {
-		// TODO Auto-generated method stub
+		disciplinas.remove(disciplina);
 		
 	}
 
