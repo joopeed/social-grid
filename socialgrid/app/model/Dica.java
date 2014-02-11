38b3ee7e412c5224e69cbe402302aad35a184cb6
Uian Sol <sol.uian@gmail.com>
2014-02-11 15:05:47 -0300

    Classe Dica - Implementação de testes.

---
+++ socialgrid/app/model/Dica.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Dica {
+
+	private String texto;
+	
+	public Dica(String novo_texto) {
+		texto = novo_texto;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dica other = (Dica) obj;
+		if (texto == null) {
+			if (other.texto != null)
+				return false;
+		} else if (!texto.equals(other.texto))
+			return false;
+		return true;
+	}
+	
+}
---
+++ socialgrid/test/model/DicaTest.java
@@ -0,0 +1,40 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DicaTest {
+
+	private Disciplina disciplina;
+	
+	@Before
+	public void SetUp() {
+		disciplina = new Disciplina("d1", 4);
+	}
+	
+	@Test
+	public void AdicionaDicasEmDisciplina() {
+		Dica dica1 = new Dica("Trancar!");
+		Dica dica2 = new Dica("Estudar pelo livro.");
+		Dica dica3 = new Dica("Tirar dúvidas pela lista.");
+		List<Dica> dicas = new ArrayList<Dica>(); 
+				
+		assertTrue(dicas.equals(disciplina.getDicas()));
+		
+		disciplina.addDica(dica2);
+		disciplina.addDica(dica3);
+		disciplina.addDica(dica1);
+		
+		dicas.add(dica2);
+		dicas.add(dica3);
+		dicas.add(dica1);
+		
+		assertTrue(dicas.equals(disciplina.getDicas()));
+	}
+
+}
