cdbfceaee0ce9c61e417447533da9557990543a1
Uian Sol <sol.uian@gmail.com>
2014-02-11 14:06:59 -0300

    Testes da classe Periodo.

---
+++ socialgrid/test/model/PeriodoTest.java
@@ -0,0 +1,56 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controllers.Controlador;
+
+public class PeriodoTest {
+
+	private Grade grade;
+	private Periodo primeiroPeriodo;
+	
+	@Before
+	public void setUp() throws IOException {
+		grade = Grade.getInstancia();
+		primeiroPeriodo = new Periodo();
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("cálculo diferencial e integral i"));
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("álgebra vetorial e geometria analítica"));
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("leitura e produção de textos"));
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("programação i"));
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("introdução à computação"));
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("laboratório de programação i"));		
+	}
+	
+	@Test
+	public void testaSeDisciplinasEstaoNoPeriodo() {
+		Set<Disciplina> disciplinas = new HashSet<Disciplina>();
+		
+		disciplinas.add(grade.getDisciplinaPorNome("cálculo diferencial e integral i"));
+		disciplinas.add(grade.getDisciplinaPorNome("álgebra vetorial e geometria analítica"));
+		disciplinas.add(grade.getDisciplinaPorNome("leitura e produção de textos"));
+		disciplinas.add(grade.getDisciplinaPorNome("programação i"));
+		disciplinas.add(grade.getDisciplinaPorNome("introdução à computação"));
+		disciplinas.add(grade.getDisciplinaPorNome("laboratório de programação i"));
+		
+		assertTrue(disciplinas.equals(primeiroPeriodo.getDisciplinas()));
+	}
+	
+	@Test
+	public void adicionaDisciplina() {
+		primeiroPeriodo.adicionaDisciplina(grade.getDisciplinaPorNome("inglês"));
+		assertTrue(primeiroPeriodo.getDisciplinas().contains(grade.getDisciplinaPorNome("inglês")));
+	}
+	
+	@Test
+	public void removeDisciplina() {
+		primeiroPeriodo.removeDisciplina(grade.getDisciplinaPorNome("cálculo diferencial e integral i"));
+		assertFalse(primeiroPeriodo.getDisciplinas().contains(grade.getDisciplinaPorNome("inglês")));
+	}
+}
