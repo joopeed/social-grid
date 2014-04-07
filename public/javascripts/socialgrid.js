a24e8a839d89d06359b71ef8c675eafa6cd0271c
Fábio Silva <fabio.fernando.osilva@gmail.com>
2014-04-06 22:51:24 -0300

    Botão de desalocar

--- public/javascripts/socialgrid.js
+++ public/javascripts/socialgrid.js
@@ -5,3 +5,13 @@ function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 } 
+
+function desalocar(disciplina) {
+	var transfere = window.confirm("Ao desalocar esta disciplina você estará desalocando" +
+								   "\nas disciplinas dela dependentes");
+    if (transfere) {
+    	location.href = '/aplicacao/desalocarDisciplina/' + disciplina;
+	} else {
+    	location.href = '/'
+	}
+}
